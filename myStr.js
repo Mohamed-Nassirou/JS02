555657a877c209241f6d0f2f972556b1d79a7529
mohamednassirouahamadou9696@gmail.com <mohamednassirouahamadou9696@gmail.com>
2023-02-14 14:10:18 +0100

    JS02

---
+++ myStr.js
@@ -0,0 +1,4 @@
+let myStr = " This is the start";
+let myStr2 = myStr + " This is the end ";
+
+console.log(myStr2);
---
+++ nameLength.js
@@ -0,0 +1,4 @@
+let nameLength = 0;
+const lastName = 'Hadiza';
+nameLength = lastName;
+console.log(nameLength.length); 
